0dc7d17a84ade13b8a07774efc41ef727f6c207a
Nikita Kuzenny <nikitakuzenny@gmail.com>
2022-12-01 21:02:00 +0200

    Serialization

--- SubscriptionService/SubServ/admin.py
+++ SubscriptionService/SubServ/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Provider, Product, Service, Subscription
+admin.site.register(User)
+admin.site.register(Provider)
+admin.site.register(Product)
+admin.site.register(Service)
+admin.site.register(Subscription)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name')
--- SubscriptionService/SubServ/views.py
+++ SubscriptionService/SubServ/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from .models import User, Provider, Product, Service, Subscription
+from .serializer import UserSerializer, ProviderSerializer, ProductSerializer, ServiceSerializer, SubscriptionSerializer
+
+class UserAPIViev(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProviderAPIViev(generics.ListAPIView):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+class ProductAPIViev(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ServiceAPIViev(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class SubscriptionAPIViev(generics.ListAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
